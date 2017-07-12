a84535ab112d724b35e31dde2e8c993cc5f64d3d
Guernoug Marwan <guernougmarwan@hotmail.fr>
2017-07-13 00:49:18 +0200

    Ajout des événements pour se déplacer sur l'écran

--- main.cpp
+++ main.cpp
@@ -16,8 +16,26 @@ const int SCREEN_HEIGHT = 768;
 using std::cout;
 using std::endl;
 
+// En attendant la classe texture
+SDL_Surface* chargementSurface(std:: string chemin)
+{
+	SDL_Surface* surface = IMG_Load(chemin.c_str());
+	return surface;
+}
+
 int main(int argc, char* args[])
 {
+	// Enumération des touches [ DEFAULT = 0, HAUT = 1 ... TOTAL = 5]
+	enum Touche
+	{
+		DEFAULT,
+		HAUT,
+		BAS,
+		GAUCHE,
+		DROITE,
+		TOTAL
+	};
+
     //  The SDL_Window object is used to create a window
     SDL_Window* window = nullptr;
 
@@ -25,10 +43,16 @@ int main(int argc, char* args[])
     SDL_Surface* screen_surface = nullptr;
 
 	// Initialisation des images
-	SDL_Surface *firstImage = nullptr, *sprite = nullptr;
+	SDL_Surface *firstImage = nullptr, *sprite = nullptr, *touchePresse[TOTAL];
 
 	// Initialisation de la position de l'image
-	SDL_Rect positionFirstImage;
+	SDL_Rect positionFirstImage,positionSprite;
+
+	// Boucle qui tourne pour chaque event
+	bool boucle = true;
+
+	// Initialisation d'un évenement "e" 
+	SDL_Event e;
 
     //  Initialisation
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -39,7 +63,7 @@ int main(int argc, char* args[])
     {
         //  Create window
 
-        window = SDL_CreateWindow("Les Terres Voilées", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow("Les Terres Voilées", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 
         if(window == nullptr)
         {
@@ -71,6 +95,9 @@ int main(int argc, char* args[])
 			// Chargement image png
 			sprite = IMG_Load("sprite.png");
 
+			// Optimisation de l'image (à voir pour les grosses images)
+			sprite = SDL_ConvertSurfaceFormat(sprite, SDL_PIXELFORMAT_RGBA8888,0);
+
 			// Si l'image a une erreur
 			if (sprite == NULL)
 			{
@@ -81,19 +108,78 @@ int main(int argc, char* args[])
 			// Position x et y des images
 			positionFirstImage.x = SCREEN_WIDTH / 4;
 			positionFirstImage.y = SCREEN_HEIGHT / 4;
+			
+			positionSprite.x = 0;
+			positionSprite.y = 0;
 
 			// Blitage de la surface
 			SDL_BlitSurface(firstImage, NULL, screen_surface, &positionFirstImage);
-			SDL_BlitSurface(sprite, NULL, screen_surface, NULL);
+			SDL_BlitSurface(sprite, NULL, screen_surface, &positionSprite);
 
 			//  Updating surface
 			SDL_UpdateWindowSurface(window);
 
-            // Wait five seconds
-            SDL_Delay(5000);
-
-
     }
+
+	// Chargement des textures de déplacement
+	touchePresse[DEFAULT] = chargementSurface("sprite.png");
+	touchePresse[HAUT] = chargementSurface("sprite.png");
+	touchePresse[BAS] = chargementSurface("sprite.png");
+	touchePresse[GAUCHE] = chargementSurface("sprite.png");
+	touchePresse[DROITE] = chargementSurface("sprite.png");
+
+
+	while (boucle)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				boucle = false;
+			}
+
+			if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_UP:
+					sprite = touchePresse[HAUT];
+					positionSprite.y-=10;
+					break;
+
+				case SDLK_DOWN:
+					sprite = touchePresse[BAS];
+					positionSprite.y+=10;
+					break;
+
+				case SDLK_LEFT:
+					sprite = touchePresse[GAUCHE];
+					positionSprite.x-=10;
+					break;
+
+				case SDLK_RIGHT:
+					sprite = touchePresse[DROITE];
+					positionSprite.x+=10;
+					break;
+
+				default:
+					sprite = touchePresse[DEFAULT];
+					break;
+				}
+			}
+		}
+
+		//  Fill the sufrace white
+		SDL_FillRect(screen_surface, nullptr, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+
+		// Blitage de la surface
+		SDL_BlitSurface(firstImage, NULL, screen_surface, &positionFirstImage);
+		SDL_BlitSurface(sprite, NULL, screen_surface, &positionSprite);
+
+		//  Updating surface
+		SDL_UpdateWindowSurface(window);
+
+	}
 
 	// Destroy Image
 	SDL_FreeSurface(firstImage);
@@ -107,3 +193,4 @@ int main(int argc, char* args[])
 
     return 0;
 }
+
